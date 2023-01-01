0c2bbf87cbcf0ed074cd4c538eb247522c56cdad
Vimo <vimal9314@gmail.com>
2023-01-01 21:45:16 +0530

    Update config.php
    changes

--- config.php
+++ config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website',
+        'name' => 'Simplee PHP Website',
         'nav_menu' => [
             '' => 'Home',
             'content/about-us.php' => 'About Us',
